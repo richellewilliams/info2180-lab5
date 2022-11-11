de66e4de0df5c9b47c0e1baeb9d7485118b7f896
Richelle Williams <richellewilliams3@gmail.com>
2022-11-10 19:39:17 -0500

    Added code so that country information is displayed in a table

--- world.php
+++ world.php
@@ -11,8 +11,22 @@ $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+<table>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Continent</th>
+      <th>Independence</th>
+      <th>Head of State</th>
+    </tr>
+  <tbody>
+    <tr>
+    <?php foreach ($results as $row): ?>
+      <td><?= $row['name']?></td>
+      <td><?= $row['continent']?></td>
+      <td><?= $row['independence_year']?></td>
+      <td><?= $row['head_of_state']?></td>
+    </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
